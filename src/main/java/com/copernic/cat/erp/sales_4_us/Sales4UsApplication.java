0a3c314232175c5663a6aafa38807bf58e220397
lucaso1424 <lucaspadilla648@gmail.com>
2022-03-03 10:00:21 +0100

    lph - Initial commit
    Projecte Spring Boot creat

---
+++ src/main/java/com/copernic/cat/erp/sales_4_us/Sales4UsApplication.java
@@ -0,0 +1,13 @@
+package com.copernic.cat.erp.sales_4_us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sales4UsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sales4UsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/copernic/cat/erp/sales_4_us/Sales4UsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copernic.cat.erp.sales_4_us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sales4UsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
