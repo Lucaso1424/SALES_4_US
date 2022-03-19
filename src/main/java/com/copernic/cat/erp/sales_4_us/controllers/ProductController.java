30b40960f90d6f92cb067f36dee67d57ee756576
lucaso1424 <lucaspadilla648@gmail.com>
2022-03-19 14:44:51 +0100

    lph - view product done
    Falta insertar bbdd camps

---
+++ src/main/java/com/copernic/cat/erp/sales_4_us/controllers/ProductController.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.copernic.cat.erp.sales_4_us.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author lucas
+ */
+@Controller
+public class ProductController {
+
+    @GetMapping("/product")
+    public String iniciGet() {
+        return "product";
+    }
+    
+    @PostMapping("/product")
+    public String inici() {
+        return "product";
+    }
+}
