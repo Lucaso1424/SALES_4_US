7030fa64aa7a240c450c1148954d39c325c6ffb3
Jose Antonio <miranda.segura.jose.antonio@alumnat.copernic.cat>
2022-03-21 09:48:24 +0100

    jquery

--- src/main/java/com/copernic/cat/erp/sales_4_us/controllers/RegisterController.java
+++ src/main/java/com/copernic/cat/erp/sales_4_us/controllers/RegisterController.java
@@ -12,11 +12,20 @@ import com.copernic.cat.erp.sales_4_us.utils.Utilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Controller
@@ -37,7 +46,12 @@ public class RegisterController {
 
     //Register process controller
     @PostMapping("process_register")
-    public String processRegistration(User user, Errors errors, RedirectAttributes msg){
+    public String processRegistration(
+            User user,
+            Errors errors,
+            RedirectAttributes msg,
+            @RequestParam("fileImage") MultipartFile multipartFile
+    ) throws IOException {
         if (errors.hasErrors()) {
             System.out.println(errors.getAllErrors());
             return "error";
@@ -55,10 +69,34 @@ public class RegisterController {
             return "redirect:/register";
         }
 
+        String fileName;
+        if (multipartFile.getOriginalFilename() == null) {
+            fileName = "hello";
+            System.out.println("Se queda aqui");
+        } else {
+            fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setFileImage(fileName);
+        }
+
         // Ecrypt password
         user.setPassword(u.encryptPass(user.getPassword()));
         user.setRol("admin");
-        repo.save(user);
+        User userSave = repo.save(user);
+        String uploadDir = "./user-logos/" + userSave.getUserId();
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)){
+            Files.createDirectories(uploadPath);
+        }
+        try (InputStream inputStream = multipartFile.getInputStream()){
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new IOException("Could not save uploaded file:" + fileName);
+        }
+
+
+
+
         return "register_success";
     }
 
--- src/main/java/com/copernic/cat/erp/sales_4_us/models/User.java
+++ src/main/java/com/copernic/cat/erp/sales_4_us/models/User.java
@@ -51,8 +51,8 @@ public class User implements Serializable {
     @Column(nullable = false, length = 10)
     String rol;
 
-    @Column(length = 45)
-    private String image;
+    @Column(length = 64)
+    private String fileImage;
 
     @Override
     public String toString(){
