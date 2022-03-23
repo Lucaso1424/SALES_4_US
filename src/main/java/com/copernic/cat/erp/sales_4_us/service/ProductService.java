2dbfc12f5b2290b6742400b4c9157aa9ad689d74
lucaso1424 <lucaspadilla648@gmail.com>
2022-03-23 02:08:07 +0100

    lph - prova print products

---
+++ src/main/java/com/copernic/cat/erp/sales_4_us/service/ProductService.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.copernic.cat.erp.sales_4_us.service;
+
+import com.copernic.cat.erp.sales_4_us.models.Product;
+import com.copernic.cat.erp.sales_4_us.repository.ProductRepository;
+import com.copernic.cat.erp.sales_4_us.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author lucas
+ */
+
+public class ProductService implements ProductServiceInterface {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Product> llistarProduct() {
+        return (List<Product>) productRepository.findAll(); 
+    }
+    
+}
---
+++ src/main/java/com/copernic/cat/erp/sales_4_us/service/ProductServiceInterface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.copernic.cat.erp.sales_4_us.service;
+
+import com.copernic.cat.erp.sales_4_us.models.Product;
+import java.util.List;
+
+/**
+ *
+ * @author lucas
+ */
+public interface ProductServiceInterface {
+
+    public List<Product> llistarProduct(); 
+
+    /*public void afegirProduct(Product pro); 
+
+    public void eliminarProduct(Product pro); 
+
+    public Product cercarProduct(Product pro);*/
+}
