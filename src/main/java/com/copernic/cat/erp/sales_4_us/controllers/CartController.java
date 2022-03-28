7a3fc534a6aaeeea7ff0d111c6d733e68b1d9269
lucaso1424 <lucaspadilla648@gmail.com>
2022-03-28 14:08:49 +0200

    lph - add product to cart done + css product
    Falta corregir: !product.empty - thymeleaf

--- src/main/java/com/copernic/cat/erp/sales_4_us/controllers/CartController.java
+++ src/main/java/com/copernic/cat/erp/sales_4_us/controllers/CartController.java
@@ -1,12 +1,27 @@
 package com.copernic.cat.erp.sales_4_us.controllers;
 
+import com.copernic.cat.erp.sales_4_us.models.Product;
+import com.copernic.cat.erp.sales_4_us.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CartController {
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/cart")
     public String inici() {
+        return "cart";
+    }
+
+    @GetMapping("/cart/{id}")
+    public String showProduct(Product product, Model model) {
+        Product p = productService.findProduct(product);
+        model.addAttribute("product", p);
         return "cart";
     }
 }
