d89a325f3bd5979d3ce20f4f91008e62df3fab65
tabasin <cpau200@gmail.com>
2022-03-29 23:41:10 +0200

    Profile

--- src/main/java/com/copernic/cat/erp/sales_4_us/controllers/ProfileController.java
+++ src/main/java/com/copernic/cat/erp/sales_4_us/controllers/ProfileController.java
@@ -1,12 +1,33 @@
 package com.copernic.cat.erp.sales_4_us.controllers;
 
+import com.copernic.cat.erp.sales_4_us.models.User;
+import com.copernic.cat.erp.sales_4_us.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 @Controller
 public class ProfileController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/profile")
-    public String inici() {
+    public String inici(Principal principal, Model model) {
+        User user = new User();
+        var users = userService.listUsers();
+        for (var u: users) {
+            if (u.getEmail().equals(principal.getName())){
+                user = u;
+            }
+        }
+        model.addAttribute("user", user);
         return "profile";
     }
 }
