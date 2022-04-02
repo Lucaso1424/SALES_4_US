0e6f6836f8358f2673ae53918f326b2c5965dec8
Jose Antonio <miranda.segura.jose.antonio@alumnat.copernic.cat>
2022-04-03 00:52:16 +0200

    editForms updated

--- src/main/java/com/copernic/cat/erp/sales_4_us/controllers/CRUDClientController.java
+++ src/main/java/com/copernic/cat/erp/sales_4_us/controllers/CRUDClientController.java
@@ -11,14 +11,22 @@ import com.copernic.cat.erp.sales_4_us.utils.Utilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 /**
- *
  * @author lucas
  */
 @Controller
@@ -38,7 +46,7 @@ public class CRUDClientController {
     }
 
     @GetMapping("/delete/{userId}")
-    public String deleteClient(User user){
+    public String deleteClient(User user) {
         userService.deleteUser(user);
         return "redirect:/crud_client";
     }
@@ -50,11 +58,62 @@ public class CRUDClientController {
         return "formClient";
     }
 
+    @GetMapping("/formEditClient")
+    public String editClientFrom(User user, Model model) {
+        User u = userService.searchUser(user);
+        model.addAttribute("user", u);
+        return "formEditClient";
+    }
+
+    @PostMapping("saveEditClient")
+    public String saveEditClient(
+            User user,
+            Errors errors,
+            @RequestParam("fileImage") MultipartFile multipartFile
+    ) throws IOException {
+        if (errors.hasErrors()) {
+            System.out.println(errors);
+            return "/profile";
+        }
+        String fileName = null;
+        if (multipartFile.getOriginalFilename() != null || !multipartFile.isEmpty()) {
+            fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setImage(fileName);
+            String uploadDir = "./src/main/resources/static/images/user-image/" + user.getEmail();
+            Path uploadPath = Paths.get(uploadDir);
+            try (InputStream inputStream = multipartFile.getInputStream()) {
+                Path filePath = uploadPath.resolve(fileName);
+                Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException ioException) {
+                throw new IOException("Could not save img " + fileName);
+            }
+        }
+        Utilities u = new Utilities();
+        user.setPassword(u.encryptPass(user.getPassword()));
+        userService.addUser(user);
+        return "redirect:/crud_client";
+    }
+
     @PostMapping("/saveClient") //action = saveClient
-    public String saveClient(User user, Errors errors) {
+    public String saveClient(User user, Errors errors, @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
         if (errors.hasErrors()) {
             System.out.println(errors);
             return "formClient";
+        }
+        String fileName;
+        if (multipartFile.getOriginalFilename() != null || !multipartFile.isEmpty()) {
+            fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        } else {
+            fileName = "default_profile.png";
+        }
+        user.setImage(fileName);
+        String uploadDir = "./src/main/resources/static/images/user-image/" + user.getEmail();
+        Path uploadPath = Paths.get(uploadDir);
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioException) {
+            throw new IOException("Could not save img " + fileName);
         }
         Utilities u = new Utilities();
         user.setPassword(u.encryptPass(user.getPassword()));
--- src/main/java/com/copernic/cat/erp/sales_4_us/controllers/ProfileController.java
+++ src/main/java/com/copernic/cat/erp/sales_4_us/controllers/ProfileController.java
@@ -8,9 +8,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
 public class ProfileController {
@@ -27,10 +37,27 @@ public class ProfileController {
     }
 
     @PostMapping("/updateProfile")
-    public String updateProfile(User user, Errors errors){
+    public String updateProfile(
+            User user,
+            Errors errors,
+            @RequestParam("fileImage") MultipartFile multipartFile
+    ) throws IOException {
         if (errors.hasErrors()) {
             System.out.println(errors);
             return "/profile";
+        }
+        String fileName = null;
+        if (multipartFile.getOriginalFilename() != null || !multipartFile.isEmpty()) {
+            fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setImage(fileName);
+            String uploadDir = "./src/main/resources/static/images/user-image/" + user.getEmail();
+            Path uploadPath = Paths.get(uploadDir);
+            try (InputStream inputStream = multipartFile.getInputStream()) {
+                Path filePath = uploadPath.resolve(fileName);
+                Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException ioException) {
+                throw new IOException("Could not save img " + fileName);
+            }
         }
         Utilities u = new Utilities();
         user.setPassword(u.encryptPass(user.getPassword()));
